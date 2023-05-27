c51db5f5af19f2d71204e24db5ad83804cf388c9
GithubAgitator <103563978+GithubAgitator@users.noreply.github.com>
2023-05-27 22:28:24 +0300

    first commit

--- l1/test_main_page.py
+++ l1/test_main_page.py
@@ -1,8 +1,16 @@
+import time
+
 from selenium.webdriver.common.by import By
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+link = "http://selenium1py.pythonanywhere.com/"
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser):
+
+    browser.get(link)
+    time.sleep(10)
+    go_to_login_page(browser)
+
