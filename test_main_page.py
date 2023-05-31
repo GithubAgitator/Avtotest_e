d375765d64452e8681eaa8dde4033213ba7be3b6
GithubAgitator <103563978+GithubAgitator@users.noreply.github.com>
2023-05-31 21:52:00 +0300

    first commit

---
+++ test_main_page.py
@@ -0,0 +1,12 @@
+import time
+from .page.main_page import MainPage
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()                      # открываем страницу
+    page.go_to_login_page()
+
+
+
+
